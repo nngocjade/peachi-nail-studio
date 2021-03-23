f69b9d6099613ac09460d3329209b53e2f72525e
nngocjade <nngocjade@gmail.com>
2021-03-23 15:42:39 +0700

    added login page and form

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,13 +1,37 @@
 import React from "react";
-import { NavLink } from "react-bootstrap";
+import "../css/Login.css";
+import { Form, NavLink, Button, Container, Row, Col } from "react-bootstrap";
 
 const LoginPage = () => {
   return (
-    <div>
+    <div className="login text-center">
       <p>login page</p>
       <p>
         Need an account? <NavLink href="/register">Create one.</NavLink>
       </p>
+      <Container className="">
+        <Row>
+          <Col>
+            <Form className="col-7 mx-auto">
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
