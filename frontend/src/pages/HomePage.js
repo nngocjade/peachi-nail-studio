13b2a90b36a074ebdac6c8e81ba72d7306d6229b
nngocjade <nngocjade@gmail.com>
2021-03-23 17:53:18 +0700

    added another call to action button

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,19 +1,20 @@
 import React from "react";
 import "../css/Home.css";
 import Logo from "../peach.svg";
-import { Container } from "react-bootstrap";
+import { Container, Button } from "react-bootstrap";
 
 const HomePage = () => {
   return (
     <Container className="home" fluid="md">
       <div className="col-5">
         <h1>Just Peachi</h1>
-        <h2>inspired by nature</h2>
+        <h2>Nails inspired by nature</h2>
         <div className="description">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate
           magnam illum minima commodi dolorem quibusdam quod hic enim
           dignissimos excepturi.
         </div>
+        <Button type="button">Sign up</Button>
       </div>
       <div>
         <img className="big-peach" src={Logo} alt="logo" />
