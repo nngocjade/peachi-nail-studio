fdd012a8e3db27fef7054516fa438f5891ed15a9
nngocjade <nngocjade@gmail.com>
2021-03-25 10:21:45 +0700

    added custom error handling

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-const nailDesigns = require("./data/nailDesigns.js");
+const NotFound = require("./middleware/errorMiddleware");
+const errorHandler = require("./middleware/errorMiddleware");
+const nailDesignRoutes = require("./routes/nailDesignRoutes");
 
 dotenv.config();
 connectDB();
 
 const app = express();
 
-app.get("/api/nailTrends", (req, res) => {
-  res.json(nailDesigns);
-});
+app.use("/api/nailDesigns", nailDesignRoutes);
+
+// MIDDLEWARE ERROR HANDLER
+app.use(NotFound.NotFound);
+
+app.use(errorHandler.errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(
