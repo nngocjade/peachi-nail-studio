c478d7841dd0ab89aa74eee7be4d51fbe3b6d7c7
nngocjade <nngocjade@gmail.com>
2021-03-28 09:39:26 +0700

    user can now register for an account

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -30,50 +30,51 @@ const LoginPage = ({ location, history }) => {
   };
 
   return (
-    <div className="login text-center">
-      <h1>Sign In</h1>
-      {error && <Message variant="danger">{error}</Message>}
-      {loading && <Loader />}
-      <p>
-        Need an account?{" "}
-        <Link to={redirect ? `/register?redirect=${redirect}` : "/register"}>
-          Create one.
-        </Link>
-      </p>
-      <Container className="">
-        <Row className="justify-content-md-center">
-          <Col xs={12} md={6} lg={4}>
-            <Form onSubmit={submitHandler}>
-              <Form.Group controlId="email">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control
-                  type="email"
-                  placeholder="Enter email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
-              </Form.Group>
+    <Container className="text-center">
+      <Row className="justify-content-md-center">
+        <Col xs={12} md={6} lg={4}>
+          <h1>Sign In</h1>
+          {error && <Message variant="danger">{error}</Message>}
+          {loading && <Loader />}
+          <p>
+            Need an account?{" "}
+            <Link
+              to={redirect ? `/register?redirect=${redirect}` : "/register"}
+            >
+              Create one.
+            </Link>
+          </p>
 
-              <Form.Group controlId="password">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  placeholder="Enter password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                ></Form.Control>
-              </Form.Group>
-              <Button variant="primary" type="submit">
-                Submit
-              </Button>
-            </Form>
-          </Col>
-        </Row>
-      </Container>
-    </div>
+          <Form onSubmit={submitHandler}>
+            <Form.Group controlId="email">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group controlId="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Enter password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -1,7 +1,110 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Row, Col, Container, Form, Button } from "react-bootstrap";
+import Message from "../components/Message.js";
+import Loader from "../components/Loader.js";
+import { Link } from "react-router-dom";
+import { register } from "../redux/actions/userActions.js";
 
-const RegisterPage = () => {
-  return <div>register page</div>;
+const RegisterPage = ({ location, history }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [message, setMessage] = useState(null);
+
+  const dispatch = useDispatch();
+
+  const userRegister = useSelector((state) => state.userRegister);
+  const { loading, error, userInfo } = userRegister;
+
+  const redirect = location.search ? location.search.split("=")[1] : "/";
+
+  useEffect(() => {
+    if (userInfo) {
+      history.push(redirect);
+    }
+  }, [history, userInfo, redirect]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      setMessage("Passwords do not match");
+    } else {
+      dispatch(register(name, email, password));
+    }
+  };
+  return (
+    <Container className="text-center">
+      <Row className="justify-content-md-center">
+        <Col xs={12} md={6} lg={4}>
+          <h1>Sign Up</h1>
+          {message && <Message variant="danger">{message}</Message>}
+          {error && <Message variant="danger">{error}</Message>}
+          {loading && <Loader />}
+          <Form onSubmit={submitHandler}>
+            {/* NAME */}
+            <Form.Group controlId="name">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="name"
+                placeholder="Enter name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            {/* EMAIL */}
+            <Form.Group controlId="email">
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            {/* PASSWORD */}
+            <Form.Group controlId="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Enter password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            {/* CONFIRM PASSWORD */}
+            <Form.Group controlId="confirmPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="confirmPassword"
+                placeholder="Confirm Password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Button type="submit" variant="primary">
+              Register
+            </Button>
+          </Form>
+
+          <Row className="py-3">
+            <Col>
+              Have an Account?{" "}
+              <Link to={redirect ? `/login?redirect=${redirect}` : "/login"}>
+                Login
+              </Link>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default RegisterPage;
