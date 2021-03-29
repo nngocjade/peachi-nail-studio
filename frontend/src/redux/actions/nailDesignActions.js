52f491bc88d4b4df365e3b9af080643fbbb57b6c
nngocjade <nngocjade@gmail.com>
2021-03-29 10:38:46 +0700

    added admin delete nial design

--- frontend/src/redux/actions/nailDesignActions.js
+++ frontend/src/redux/actions/nailDesignActions.js
@@ -3,7 +3,13 @@ import {
   NAILDESIGN_LIST_REQUEST,
   NAILDESIGN_LIST_SUCCESS,
   NAILDESIGN_LIST_FAIL,
+  NAILDESIGN_DELETE_REQUEST,
+  NAILDESIGN_DELETE_FAIL,
+  NAILDESIGN_DELETE_SUCCESS,
 } from "../constants/nailDesignConstants";
+import { logout } from "./userActions.js";
+
+// =========== LIST NAIL DESIGNS ACTION ==================
 
 export const listNailDesigns = () => async (dispatch) => {
   try {
@@ -22,6 +28,49 @@ export const listNailDesigns = () => async (dispatch) => {
         error.message && error.response.data.message
           ? error.response.data.message
           : error.message,
+    });
+  }
+};
+
+/**
+ *  =================================================================
+ * -------------------------- ADMIN ONLY -------------------------------
+ * ===================================================================
+ */
+// ================ DELETE NAIL DESIGN ACTION =================
+
+export const deleteNailDesign = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: NAILDESIGN_DELETE_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    await axios.delete(`/api/nailDesigns/${id}`, config);
+
+    dispatch({
+      type: NAILDESIGN_DELETE_SUCCESS,
+    });
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    if (message === "Not authorized, token failed") {
+      dispatch(logout());
+    }
+    dispatch({
+      type: NAILDESIGN_DELETE_FAIL,
+      payload: message,
     });
   }
 };
