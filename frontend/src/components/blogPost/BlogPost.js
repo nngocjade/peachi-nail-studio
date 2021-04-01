a60b4ebaa83a8e86b4bf38bc6019b6b2a3af459e
nngocjade <nngocjade@gmail.com>
2021-04-01 11:16:15 +0700

    added blog post card

--- frontend/src/components/blogPost/BlogPost.js
+++ frontend/src/components/blogPost/BlogPost.js
@@ -7,13 +7,10 @@ const BlogPost = ({ blogPost }) => {
   return (
     <div>
       <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src={defaultImage} />
+        <Card.Img variant="top" src={blogPost.image} />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
+          <Card.Title>{blogPost.title}</Card.Title>
+          <Card.Text>{blogPost.description}</Card.Text>
         </Card.Body>
         <Card.Body>
           <Card.Link href="#">Card Link</Card.Link>
