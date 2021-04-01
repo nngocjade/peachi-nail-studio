7222bd32bef29dfbf736a18c798e2af11cc9af96
nngocjade <nngocjade@gmail.com>
2021-04-01 11:09:22 +0700

    changed the createblogpost

--- backend/controllers/blogPostController.js
+++ backend/controllers/blogPostController.js
@@ -17,15 +17,26 @@ const getBlogPosts = asyncHandler(async (req, res) => {
 
 // ====================== CREATE BLOG POST ====================
 
+// const createBlogPost = asyncHandler(async (req, res) => {
+//   try {
+//     const blogPost = new BlogPost({
+//       title: "Sample title",
+//       description: "short description",
+//       creator: "sample creator",
+//       image: "/images/sample.jpg",
+//       likeCount: 5,
+//     });
+
+//     const createdBlogPost = await blogPost.save();
+//     res.status(201).json(createdBlogPost);
+//   } catch (error) {
+//     res.status(404).json({ message: error.message });
+//   }
+// });
+
 const createBlogPost = asyncHandler(async (req, res) => {
   try {
-    const blogPost = new BlogPost({
-      title: "Sample title",
-      description: "short description",
-      creator: "sample creator",
-      image: "/images/sample.jpg",
-      likeCount: 5,
-    });
+    const blogPost = new BlogPost(req.body);
 
     const createdBlogPost = await blogPost.save();
     res.status(201).json(createdBlogPost);
