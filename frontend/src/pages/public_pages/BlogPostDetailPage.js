32a0e3b5ab65ab78f7b0e033a3d08d95000f6624
nngocjade <nngocjade@gmail.com>
2021-04-09 10:25:07 +0700

    added modal

---
+++ frontend/src/pages/public_pages/BlogPostDetailPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BlogPostDetailPage = () => {
+  return <div>blog post detail page</div>;
+};
+
+export default BlogPostDetailPage;
--- frontend/src/pages/public_pages/NailGalleryPage.js
+++ frontend/src/pages/public_pages/NailGalleryPage.js
@@ -1,6 +1,6 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import EachDesign from "../../components/EachDesign";
 import Loader from "../../components/Loader";
 import Message from "../../components/Message";
@@ -11,8 +11,22 @@ import SearchBox from "../../components/SearchBox";
 import { Route } from "react-router";
 import "../../css/Gallery.css";
 import Paginate from "../../components/Paginate";
+import DesignDetailModal from "../../components/DesignDetailModal";
 
 const NailGalleryPage = ({ match, history }) => {
+  const [choosenId, setChoosenId] = useState("");
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => {
+    setShow(false);
+    setChoosenId("");
+  };
+  const handleShow = (id) => {
+    console.log("hererere");
+    setShow(true);
+    setChoosenId(id);
+  };
   const keyword = match.params.keyword;
 
   const pageNumber = match.params.pageNumber || 1;
@@ -36,25 +50,32 @@ const NailGalleryPage = ({ match, history }) => {
   };
 
   return (
-    <Container className="nail-gallery" fluid="md">
-      <Route render={({ history }) => <SearchBox history={history} />} />
-      {loading ? (
-        <Loader />
-      ) : error ? (
-        <Message variant="danger">{error}</Message>
-      ) : (
-        <Masonry
-          breakpointCols={breakpoints}
-          className="my-masonry-grid"
-          columnClassName="my-masonry-grid_column"
-        >
-          {nailDesigns.map((eachDesign) => (
-            <EachDesign eachDesign={eachDesign} />
-          ))}
-        </Masonry>
-      )}
-      <Paginate pages={pages} page={page} keyword={keyword ? keyword : " "} />
-    </Container>
+    <>
+      <Container className="nail-gallery" fluid="md">
+        <Route render={({ history }) => <SearchBox history={history} />} />
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant="danger">{error}</Message>
+        ) : (
+          <Masonry
+            breakpointCols={breakpoints}
+            className="my-masonry-grid"
+            columnClassName="my-masonry-grid_column"
+          >
+            {nailDesigns.map((eachDesign) => (
+              <EachDesign eachDesign={eachDesign} handleShow={handleShow} />
+            ))}
+          </Masonry>
+        )}
+        <Paginate pages={pages} page={page} keyword={keyword ? keyword : " "} />
+      </Container>
+      <DesignDetailModal
+        handleClose={handleClose}
+        show={show}
+        choosenId={choosenId}
+      />
+    </>
   );
 };
 
