c24a74f2d38bb33bd04b6332ec2a1fa51def17fc
nngocjade <nngocjade@gmail.com>
2021-04-10 14:18:10 +0700

    successfully added favorites

--- frontend/src/pages/user_admin_pages/ProfilePage.js
+++ frontend/src/pages/user_admin_pages/ProfilePage.js
@@ -124,21 +124,29 @@ const ProfilePage = ({ history, location }) => {
   );
 
   const favorite = (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-      </Card.Body>
+    <>
+      {!userDetails ? (
+        <Loader />
+      ) : (
+        userDetails.user.favorites.map((f) => (
+          <Card.Body style={{ width: "18rem" }}>
+            <Card.Img variant="top" src={f.image} />
+            <Card.Body>
+              <Card.Title>{f.name}</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
 
-      <Card.Body>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
-      </Card.Body>
-    </Card>
+            <Card.Body>
+              <Card.Link href="#">Card Link</Card.Link>
+              <Card.Link href="#">Another Link</Card.Link>
+            </Card.Body>
+          </Card.Body>
+        ))
+      )}
+    </>
   );
 
   return (
