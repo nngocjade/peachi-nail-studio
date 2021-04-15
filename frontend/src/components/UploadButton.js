50fcbec766b2d8e238c67130a421eca45014cb5f
nngocjade <nngocjade@gmail.com>
2021-04-15 11:11:17 +0700

    refactored nail  design edit page, replaced upload button with arrow icon

---
+++ frontend/src/components/UploadButton.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
+const UploadButton = ({ myWidget }) => {
+  return (
+    <Button onClick={() => myWidget.open()} variant="primary">
+      <i
+        class="fas fa-arrow-up"
+        style={{
+          width: "1.5em",
+          height: "1.5em",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      ></i>
+    </Button>
+  );
+};
+
+export default UploadButton;
