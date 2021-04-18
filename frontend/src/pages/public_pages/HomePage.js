3b5681cbd05969824f3fccd7492359d8d5c80392
nngocjade <nngocjade@gmail.com>
2021-04-19 00:35:54 +0700

    adding background design

--- frontend/src/pages/public_pages/HomePage.js
+++ frontend/src/pages/public_pages/HomePage.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import "../../css/Home.css";
 import Logo from "../../svg/peach.svg";
 import NailPolish from "../../svg/nail-polish.svg";
-import BackgroundPeach from "../../svg/peach(1).svg";
+import Blossom from "../../svg/cherry-blossom(3).svg";
+// import BackgroundPeach from "../../svg/peach(1).svg";
 import { Container, Button, Row, Col } from "react-bootstrap";
 
 const HomePage = () => {
@@ -19,64 +20,80 @@ const HomePage = () => {
   window.addEventListener("scroll", makeNailPolishVisible);
 
   return (
-    <Container className="home" fluid="md">
-      {/* <div className="background-peach-wrapper">
+    <>
+      <Container className="home">
+        {/* <div className="background-peach-wrapper">
         <img
           className="background-peach"
           src={BackgroundPeach}
           alt="background peach"
         />
       </div> */}
-      <div
-        className={
-          visible
-            ? "nail-polish-absolute element-visible"
-            : "nail-polish-absolute"
-        }
-      >
-        <div className="nail-polish-wrapper">
-          <img
-            className="wobble animated infinite nail-polish"
-            src={NailPolish}
-            alt="nail-polish"
-          />
-          <img
-            className="wobble animated infinite nail-polish-front"
-            src={NailPolish}
-            alt="nail-polish"
-          />
+        <div className="circle"></div>
+        <div className="circle-one"></div>
+        <div
+          className={
+            visible
+              ? "nail-polish-absolute element-visible"
+              : "nail-polish-absolute"
+          }
+        >
+          <div className="nail-polish-wrapper">
+            <img
+              className="wobble animated infinite nail-polish"
+              src={NailPolish}
+              alt="nail-polish"
+            />
+            <img
+              className="wobble animated infinite nail-polish-front"
+              src={NailPolish}
+              alt="nail-polish"
+            />
+          </div>
         </div>
-      </div>
-      <Row>
-        <Col className="col-5 landing-content">
-          <h1>Just Peachi</h1>
-          <h4 style={{ padding: ".5em 1em" }}>inspired by joy</h4>
-          <div className="description" style={{ paddingBottom: "1em" }}>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate
-            magnam illum minima commodi dolorem quibusdam quod hic enim
-            dignissimos excepturi.
+        <Row>
+          <Col className="col-5 landing-content">
+            <h1>Just Peachi</h1>
+            <h4 style={{ padding: ".5em 1em" }}>inspired by joy</h4>
+            <div className="description" style={{ paddingBottom: "1em" }}>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Voluptate magnam illum minima commodi dolorem quibusdam quod hic
+              enim dignissimos excepturi.
+            </div>
+            <button
+              type="button"
+              className="learn-more btn"
+              style={{ width: "14em", margin: "1em" }}
+            >
+              <span>Learn more</span>
+            </button>
+          </Col>
+          <Col className="peach-col">
+            <div className="peach-wrapper">
+              <img className="big-peach" src={Logo} alt="logo" />
+              <img className="big-peach-front blink_me" src={Logo} alt="logo" />
+            </div>
+          </Col>
+        </Row>
+        <Row className="services text-center ">
+          <div className="blossom-wrapper">
+            <img className="blossom" src={Blossom} alt="blossom" />
           </div>
-          <button
-            type="button"
-            className="learn-more btn"
-            style={{ width: "14em", margin: "1em" }}
-          >
-            <span>Learn more</span>
-          </button>
-        </Col>
-        <Col className="peach-col">
-          <div className="peach-wrapper">
-            <img className="big-peach" src={Logo} alt="logo" />
-            <img className="big-peach-front blink_me" src={Logo} alt="logo" />
+          <div className="blossom-wrapper-one ">
+            <img className="blossom-one" src={Blossom} alt="blossom" />
           </div>
-        </Col>
-      </Row>
-      <Row className="services text-center ">
-        <Col>Mani / Pedi</Col>
-        <Col>Nail Design</Col>
-        <Col>Gel</Col>
-      </Row>
-    </Container>
+          <Col sm={12} md={6} lg={4}>
+            <div className="card">Mani / Pedi</div>
+          </Col>
+          <Col sm={12} md={6} lg={4}>
+            <div className="card">Nail Design</div>
+          </Col>
+          <Col sm={12} md={6} lg={4}>
+            <div className="card">Gel</div>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
